3f608027a3de71f891cc63e78da909d19df978b5
Diego Arriagada <diegoarriagadazamora@gmail.com>
2022-07-15 00:20:33 -0400

    se crea js y se añade al html

---
+++ main.js
@@ -0,0 +1,8 @@
+alert("Hola, Lee la consola");
+
+let name = "Diego";
+let lastName = "Arriagada";
+console.log(name, lastName);
+
+const info = "2 let con 1 console log, y este mensaje con const";
+console.log(info);
